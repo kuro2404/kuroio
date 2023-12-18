5130a455fc2297688aa417d33ec677651aee0488
kuro2404 <veerdhawal01@gmail.com>
2023-12-18 18:25:15 +0530

    first

---
+++ src/data/WorkData.js
@@ -0,0 +1,63 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"The Tech Walk",
+        description:"Take a walkthrough of the latest trends, news in Programming and Technology , A Tech Blog",
+        tags:["nextjs","graphcms","tailwindcss"],
+        
+        demo:"https://thetechwalk.com/",
+        github:""
+    },
+    {
+        id:2,
+        name:"Leadcell B2B",
+        description:"Landing page for Leadcell B2B, a marketing and advertising company",
+        tags:["html","sass"],
+        
+        demo:"https://www.leadcellb2b.com",
+        github:""
+        
+    },
+    {
+        id:3,
+        name:"Rajya Loktantra",
+        description:"A Complete News Portal for Maharashtra",
+        tags:["express","nodejs","mongodb"],
+        
+        demo:"http://www.rajyaloktantra.com",
+        github:""
+    },
+    {
+        id:4,
+        name:"Coming Soon",
+        description:"Something real cool, upcoming",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"",
+        github:""
+       
+    },{
+        id:5,
+        name:"Coming Soon",
+        description:"Something real cool, upcoming!",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"",
+        github:""
+        
+    },
+    {
+        id:6,
+        name:"Comin coming coming sooooon",
+        description:"lol",
+        tags:["react","react-router","css"],
+        
+        demo:"",
+        github:""
+        
+    },
+    
+  
+] 
